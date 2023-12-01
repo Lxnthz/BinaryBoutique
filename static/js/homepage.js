2b5a3adcd61f8ae4d45d57feac7475eccde83ab4
dsputra12 <142730612+dsputra12@users.noreply.github.com>
2023-12-02 02:31:10 +0700

    Add files via upload

---
+++ static/js/homepage.js
@@ -0,0 +1,54 @@
+// QUOTES MACHINE -------------------------------------------------------------
+const quotes = [
+    { 
+        text: "Coding like poetry should be short and concise.", 
+        creator: "Santosh Kalwar" 
+    },
+    { 
+        text: "First, solve the problem. Then, write the code.", 
+        creator: "John Johnson" 
+    },
+    { 
+        text: "Code is like humor. When you have to explain it, it’s bad.", 
+        creator: "Cory House" 
+    },
+    { 
+        text: "Make it work, make it right, make it fast.", 
+        creator: "Kent Beck" 
+    },
+    { 
+        text: "Clean code always looks like it was written by someone who cares.", 
+        creator: "Robert C. Martin" 
+    },
+    { 
+        text: "Of course, bad code can be cleaned up. But it’s very expensive.", 
+        creator: "Robert C. Martin" 
+    },
+];
+function generateQuote() {
+    const quoteText = document.getElementById('quote-text');
+    const quoteCreator = document.getElementById('quote-creator');
+    const randomIndex = Math.floor(Math.random() * quotes.length);
+
+    quoteText.style.opacity = 0;
+    quoteCreator.style.opacity = 0;
+
+    setTimeout(() => {
+        quoteText.textContent = quotes[randomIndex].text;
+        quoteCreator.textContent = `- ${quotes[randomIndex].creator} -`;
+        quoteText.style.opacity = 1;
+        quoteCreator.style.opacity = 1;
+    }, 500);
+}
+function showCreator() {
+    const quoteCreator = document.getElementById('quote-creator');
+    quoteCreator.style.opacity = 1;
+}
+function hideCreator() {
+    const quoteCreator = document.getElementById('quote-creator');
+    quoteCreator.style.opacity = 0;
+}
+generateQuote();
+setInterval(generateQuote, 10000);
+
+// FAQ DROPDOWN -------------------------------------------------------------
---
+++ static/js/user.js
@@ -0,0 +1,20 @@
+const search = () => {
+    const searchBox = document.getElementById("search-bar").value.toUpperCase();
+    const langList = document.getElementById("lang-container");
+    const languages = document.querySelectorAll(".content");
+    const lname = document.getElementsByTagName("h3")
+
+    for(var i = 0; i < lname.length; i++) {
+        let match = languages[i].getElementsByTagName('h3')[0];
+
+        if(match) {
+            let textValue = match.textContent || match.innerHTML
+
+            if(textValue.toUpperCase().indexOf(searchBox) > -1) {
+                languages[i].style.display = "";
+            } else {
+                languages[i].style.display = "none";
+            }
+        }
+    }
+}
